2ae8e93b91db1830d47a438a0675f6a11152358b
kalmch <44063147+kalmch@users.noreply.github.com>
2019-05-14 19:36:28 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.ck.almos.menuexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.mymenu, m);
+        return true;
+    }
+}
